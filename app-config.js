5f5fa12391dbe6f6e57df1a5391a297f5c2535f8
amrahmed865-lab <amrahmed865@gmail.com>
2026-06-22 03:39:57 +0300

    Add files via upload

--- app-config.js
+++ app-config.js
@@ -5,3 +5,7 @@
 // لإيقاف صفحة الطلبات مؤقتاً: REQUESTS_ENABLED = false
 // لإعادة تشغيلها: REQUESTS_ENABLED = true
 const REQUESTS_ENABLED = false;
+
+// لإيقاف لوحة التحكم مؤقتاً: DASHBOARD_ENABLED = false
+// لإعادة تشغيلها: DASHBOARD_ENABLED = true
+const DASHBOARD_ENABLED = false;
