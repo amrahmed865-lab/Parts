3e6243265bd160bb372643655fc41ddf32a544d4
amrahmed865-lab <amrahmed865@gmail.com>
2026-06-24 19:08:57 +0300

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,43 +1,37 @@
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
 firebase.initializeApp({
- apiKey: "AIzaSyDZ1RAn4fIN6Rqc-b5-BoHiiNFQ9GvZZJA",
-  authDomain: "parts-7e82e.firebaseapp.com",
-  projectId: "parts-7e82e",
-  storageBucket: "parts-7e82e.firebasestorage.app",
-  messagingSenderId: "198514736431",
-  appId: "1:198514736431:web:3e88fb478f4cfc910035da"
+
+  apiKey: "AIzaSyCk-sP0Z-TGyx1rcyQhXFR1D52BY-aLt1E",
+
+  authDomain: "maintenance-nahda.firebaseapp.com",
+
+  projectId: "maintenance-nahda",
+
+  storageBucket: "maintenance-nahda.firebasestorage.app",
+
+  messagingSenderId: "1055981144653",
+
+  appId: "1:1055981144653:web:cecf0a83969248f7d12a04"
+
 });
 
 const messaging = firebase.messaging();
 
 messaging.setBackgroundMessageHandler(function(payload) {
 
-  const partId = payload.data?.partId || "";
-
   return self.registration.showNotification(
+
     payload.notification.title,
+
     {
       body: payload.notification.body,
       icon: '/icon-192.png',
-      badge: '/icon-192.png',
-      data: {
-        url: partId
-          ? `/parts.html?part=${partId}`
-          : '/parts.html'
-      }
+      badge:'/icon-192.png'
     }
+
   );
-});
 
-self.addEventListener('notificationclick', function(event) {
-  event.notification.close();
-
-  const targetUrl =
-    event.notification.data?.url || '/parts.html';
-
-  event.waitUntil(
-    clients.openWindow(targetUrl)
-  );
 });
